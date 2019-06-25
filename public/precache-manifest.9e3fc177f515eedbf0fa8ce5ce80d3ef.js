5e9e9116af7b4b3c7f0ce3ed4691b5933b5f278f
mannvender <mannvender001@gmail.com>
2019-06-25 22:21:20 +0530

    make it better

---
+++ public/precache-manifest.9e3fc177f515eedbf0fa8ce5ce80d3ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6131e6fd60de33deebc1",
+    "url": "/static/css/main.403015d3.chunk.css"
+  },
+  {
+    "revision": "6131e6fd60de33deebc1",
+    "url": "/static/js/main.41a521db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "27c6600d544731475bff",
+    "url": "/static/css/2.fc612701.chunk.css"
+  },
+  {
+    "revision": "27c6600d544731475bff",
+    "url": "/static/js/2.432478b4.chunk.js"
+  },
+  {
+    "revision": "aa908c1510ee314b7f5731852650abb7",
+    "url": "/index.html"
+  }
+];
